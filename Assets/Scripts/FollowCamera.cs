b168d6d421bdbaf99166a1dfbaa0537b9494a090
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2023-01-23 10:20:20 +0100

    consigo que la camara siga al jugador

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x,y,z;
+        x = Mathf.Clamp(player.position.x, 0f,105.09f);
+        y = Mathf.Clamp(player.position.y, 0f,4.79f);
+        z = transform.position.z;
+        transform.position = new Vector3(x,y,z);
+    }
+}
