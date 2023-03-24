1c1180a344952f39a6908d89b009f868e4f7e669
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2023-03-24 17:13:50 +0100

    consito instanciar prefabs!!!!

--- Assets/Tests_Edit_Mode/CheckPointTest.cs
+++ Assets/Tests_Edit_Mode/CheckPointTest.cs
@@ -6,22 +6,21 @@ using UnityEngine.TestTools;
 
 public class CheckPointTest
 {
-    
-    // A Test behaves as an ordinary method
-    [Test]
-    public void CheckPointTestSimplePasses()
+    GameObject checkpoint;
+    [SetUp]
+    public void Setup()
     {
-        
-        // Use the Assert class to test conditions
+        GameObject checkpoint = Resources.Load<GameObject>("Prefabs/CheckPoint");
+
+        GameObject checkpointInstance = Object.Instantiate(checkpoint, Vector3.zero, Quaternion.identity);
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator CheckPointTestWithEnumeratorPasses()
+    public IEnumerator TestPrefab()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
+
+
+
         yield return null;
     }
 }
