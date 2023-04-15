ae3ea62de2f92d205d5a458bbfa5a8e05774c8ab
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2023-04-15 13:48:47 +0200

    menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void LoadFirstScene(){
         SceneManager.LoadScene(1);
     }
