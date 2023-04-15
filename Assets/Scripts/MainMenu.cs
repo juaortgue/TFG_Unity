8ebc9a3572ab72c04a6d20eb744af82f9d635d30
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2023-04-15 13:48:17 +0200

    mejoro menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,21 +5,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void LoadFirstScene(){
         SceneManager.LoadScene(1);
     }
     public void ExitGame() {
-        Debug.Log("me salgo del juego");
         Application.Quit();
     }
 }
