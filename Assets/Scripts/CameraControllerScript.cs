d768a650a8fa78cf141c1a090c04a86ea15562f9
Juan Antonio Ortiz Guerra <juanantonioortizguerra@gmail.com>
2023-04-27 09:55:15 +0300

    añado retoques escenario2

--- Assets/Scripts/CameraControllerScript.cs
+++ Assets/Scripts/CameraControllerScript.cs
@@ -8,15 +8,16 @@ public class CameraControllerScript : MonoBehaviour
     public Vector3 offset;
     // Start is called before the first frame update
    
-
+    public float xLimit;
+    public float yLimit;
     // Update is called once per frame
     void Update()
     {
         if (transform != null && player!=null)
         {
             float x, y, z;
-            x = Mathf.Clamp(player.position.x, 0f, 166.05f);
-            y = Mathf.Clamp(player.position.y, 0f, 0.04f);
+            x = Mathf.Clamp(player.position.x, 0f, xLimit);
+            y = Mathf.Clamp(player.position.y, 0f, yLimit);
             z = transform.position.z;
             transform.position = new Vector3(x, y, z);
         }
